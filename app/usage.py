03385300095d3b226fc82c018c8a985f0272fa1f
zzy0302 <503652983@qq.com>
2019-10-25 17:13:50 +0800

    much feature complete

--- app/usage.py
+++ app/usage.py
@@ -5,8 +5,12 @@ from nonebot import on_command, CommandSession
 @on_command('usage', aliases=['使用帮助', '帮助', '使用方法'])
 async def _(session: CommandSession):
     # 获取设置了名称的插件列表
-    plugins = list(filter(lambda p: p.name, nonebot.get_loaded_plugins()))
+    temp=nonebot.get_loaded_plugins()
+    for i in temp:
+        print(dir(i),i.name)
 
+    plugins = list(filter(lambda p: p.name, nonebot.get_loaded_plugins()))
+    print(plugins)
     arg = session.current_arg_text.strip().lower()
     if not arg:
         # 如果用户没有发送参数，则发送功能列表
